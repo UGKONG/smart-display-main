252896f08a4e7afb136d844e682ee651e73f20ba
sanguk <jsw9330@icloud.com>
2022-04-01 17:45:22 +0900

    17:45:25

---
+++ main.js
@@ -0,0 +1,291 @@
+module.exports = { useNow, serverStart, dbConnect, clientSQL }
+
+let timeProcessCount = 0;
+let requestCount = 0;
+const ip = require('ip');
+const fs = require('fs');
+const request = require('request');
+const config_api = require('./config/api.json');
+const { db } = require('./index');
+const {
+  apiErrorCodeList,
+  sidoNameList,
+  nowWeatherCategoryList,
+  nowDustCategoryList,
+} = require('./static.json');
+
+
+// 현재 날씨 상태
+// 날짜 & 시간 셋팅
+// 등록된 장비 위치 좌표마다 날씨 정보 GET
+function getNowWeatherTimeSet () {
+  let dateTime = useNow({ hour: 0, format: false });
+  let [date, time] = dateTime.split(' ');
+  time = time.slice(0, 2);
+  time = time + '00';
+  console.log(date, time, '현재 날씨 데이터를 요청합니다.');
+
+  db.query(`
+    SELECT DISTINCT b.NX, b.NY FROM 
+    hardware_list AS a LEFT JOIN location_list AS b 
+    ON a.LOCATION_ID = b.ID
+    `, (err, result) => {
+    if (err) return log('위치 정보 조회 요청에 실패하였습니다.', err);
+  
+    result.forEach(loc => getNowWeather({ date, time, loc }));
+  });
+}
+
+// 현재 날씨 정보 요청 (기상청 API)
+function getNowWeather ({ date, time, loc }) {
+
+  let query = useQueryString({
+    ServiceKey: config_api.nowWeatherKey,
+    pageNo: 1,
+    numOfRows: 1000,
+    dataType: 'JSON',
+    base_date: date,
+    base_time: time,
+    nx: loc.NX,
+    ny: loc.NY
+  });
+
+  const validation = (data) => {
+    let body = data?.body;
+    if (typeof(data) !== 'object') return false;
+    if (!body) return false;
+    if (body.indexOf('<') > -1) return false;
+    let errorCode = JSON.parse(body)?.response?.header?.resultCode;
+    if (Number(errorCode)) {
+      apiError(errorCode);
+      return false;
+    }
+    return true;
+  }
+
+  request(`http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?${query}`,
+    (err, result) => {
+      if (err) return log('초단기실황 데이터 요청에 실패하였습니다. (날짜: ' + date + ', 시간: ' + time + ')', err);
+      log('초단기실황 데이터 요청에 성공하였습니다. (날짜: ' + date + ', 시간: ' + time + ')');
+      
+      if (!validation(result)) return console.log('데이터를 가져오지 못했습니다.');
+
+      let data = JSON.parse(result?.body)?.response?.body?.items?.item;
+      newNowWeather({ data, loc, date, time });
+    }
+  );
+}
+
+// 현재 날씨 정보 가공
+function newNowWeather ({ data, loc, date, time }) {
+
+  db.query(`
+    SELECT COUNT(a.ID) AS COUNT 
+    FROM now_weather AS a 
+    WHERE NX=${loc.NX} AND NY=${loc.NY}
+  `, (err, result) => {
+    if (err) return log('초단기실황 데이터 존재여부 요청을 실패하였습니다.', err);
+    log('초단기실황 데이터 존재여부를 조회했습니다.');
+    let COUNT = result[0]?.COUNT;
+    
+    if (!COUNT) {  // 신규 생성
+
+      let categorySQL = [];
+      nowWeatherCategoryList.forEach(item => {
+        let cate = data.find(x => x.category === item);
+        categorySQL.push(cate ? cate?.obsrValue : null);
+      });
+      
+      db.query(`
+        INSERT INTO now_weather
+        (NX,NY,DATE,TIME,${nowWeatherCategoryList.join(',')})
+        VALUES
+        (${loc.NX},${loc.NY},'${date}','${time}',${categorySQL.join(',')})
+      `, (err, result) => {
+        if (err) return log('초단기실황 신규 데이터 삽입에 실패하였습니다.', err);
+
+        log(
+          ':: ' + useNow() + ' -> 초단기실황: 새로운 데이터로 추가됨',
+          ':: ' + useNow() + ' -> 초단기실황: 새로운 데이터로 추가됨'
+        );
+      });
+
+    } else {  // 기존 수정
+      let categorySQL = [];
+      nowWeatherCategoryList.forEach(item => {
+        let cate = data.find(x => x.category === item);
+        categorySQL.push(item + '=' + (cate ? cate?.obsrValue : null));
+      });
+
+      db.query(`
+        UPDATE now_weather SET
+        DATE='${date}',TIME='${time}',${categorySQL.join(',')}
+        WHERE
+        NX=${loc.NX} AND NY=${loc.NY};
+      `, (err, result) => {
+        if (err) return log('초단기실황 신규 데이터 업데이트에 실패하였습니다', err)
+
+        log(
+          ':: ' + useNow() + ' -> 초단기실황: 새로운 데이터로 교체됨',
+          ':: ' + useNow() + ' -> 초단기실황: 새로운 데이터로 교체됨'
+        );
+      });
+    }
+  });
+}
+
+// 현재 미세먼지 정보 요청 (에어코리아 API)
+function getNowDust (item) {
+  let sidoName = sidoNameList.find(x => x.long === item.PATH1)?.short ?? '서울';
+
+  let query = useQueryString({
+    serviceKey: config_api.nowDustKey,
+    returnType: 'json',
+    numOfRows: 1000,
+    pageNo: 1,
+    sidoName: encodeURI(sidoName),
+  });
+  
+  request(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?${query}`,
+    (err, result) => {
+      if (err) return log('시도별 실시간 미세먼지 데이터 요청에 실패하였습니다. (' + item.PATH3 + ')', err);
+
+      log('시도별 실시간 미세먼지 데이터 요청에 성공하였습니다. (' + item.PATH3 + ')');
+
+      let errorCode = JSON.parse(result.body).response.header.resultCode;
+      if (Number(errorCode) > 0) return apiError(errorCode);
+
+      let data = JSON.parse(result.body).response.body.items;
+      newNowDust(data, item);
+    }
+  )
+}
+
+// 현재 미세먼지 정보 가공
+function newNowDust (data, item) {
+  // nowDustCategoryList
+  let sidoName = sidoNameList.find(x => x.short === data.sidoName)?.long ?? '서울특별시';
+  const _data = data[0];
+  // console.log(_data);
+
+  db.query(`
+    SELECT * FROM now_weather WHERE
+    LOCATION LIKE '${sidoName}%'
+  `, (err, result) => {
+    if (err) return log('시도별 실시간 미세먼지 데이터 존재여부 요청을 실패하였습니다.', err);
+
+    log('시도별 실시간 미세먼지 데이터 존재여부를 조회했습니다.');
+
+    if (result.length === 0) {  // 신규 생성
+      db.query(`
+        INSERT INTO now_weather ()
+      `, (err, result) => {
+
+      })
+    } else {  // 기존 수정
+      
+    }
+  });
+}
+
+// 클라이언트 SQL 실행 함수
+function clientSQL (req, res) {
+  let val = req.params.SQL;
+  log('Node SQL Client에서 Query문을 요청하였습니다.\nQuery: ' + val);
+  db.query(val, (err, result) => {
+    if (err) {
+      log('Node SQL Client에서 Query문 시도에 실패하였습니다.');
+      res.send(err);
+      return;
+    }
+    log('Node SQL Client에서 Query문을 요청에 성공하였습니다.');
+    res.send(result);
+  })
+}
+
+// 일정 시간마다 실행할 함수들
+function getFunctions () {
+  getNowWeatherTimeSet();
+  //
+  //
+}
+
+// Time Process
+function timeProcess () {
+
+  getNowWeatherTimeSet();
+
+  setInterval(() => {
+    let minutes = new Date().getMinutes();
+    if (minutes != config_api.getMinutes) return;
+    timeProcessCount++;
+    console.log('Time Process가 실행되었습니다.. (' + timeProcessCount + ')');
+    getFunctions();
+  }, 60000);
+}
+
+// 서버 시작 함수
+function serverStart () {
+
+  console.clear();
+  log(
+    ':: ' + useNow() + ' -> Nodejs 서버 연결 성공',
+    ':: ' + useNow() + ' -> Nodejs 서버 연결 성공'
+  );
+  timeProcess();
+}
+
+// Database 연결 함수
+function dbConnect (err) {
+  if (err) return log('데이터베이스 연결에 실패하였습니다.', err);
+  log(
+    ':: ' + useNow() + ' -> 데이터베이스 연결 성공',
+    ':: ' + useNow() + ' -> 데이터베이스 연결 성공'
+  );
+}
+
+// log.txt에 log 저장
+function log (logText = '', error) {
+  let saveText = ':: ' + useNow() + ' -> ' + logText + '\n';
+
+  fs.appendFile('./log.txt', saveText, err => {
+    err && console.log('log 저장에 실패하였습니다.', err);
+    error && console.log(error);
+  });
+}
+
+// API 에러 코드 함수
+function apiError (code) {
+  let find = apiErrorCodeList.find(x => Number(x.id) === Number(code));
+  log(
+    ':: ' + useNow() + ' -> 공공데이터 포털 API 에러\n(에러코드: ' + code + ', 메시지: ' + find.msg + ', 설명: ' + find.desc + ')',
+    { code, msg: find.msg, description: find.desc }
+  );
+}
+
+// Object -> Query (String) 변환 함수
+function useQueryString (obj) {
+  let strArr = [];
+  let keys = Object.keys(obj);
+  keys.forEach(key => strArr.push(key + '=' + obj[key]));
+  let result = strArr.join('&');
+  return result;
+}
+
+// 현재 날짜 & 시간 출력 함수
+function useNow (option = { hour: 0, format: true }) {
+  let hour = option.hour;
+  let format = option.format;
+  let date = new Date();
+  if (hour !== 0) date.setHours(date.getHours() + hour);
+  let Y = String(date.getFullYear());
+  let M = String((date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1));
+  let D = String(date.getDate() < 10 ? '0' + date.getDate() : date.getDate());
+  let h = String(date.getHours() < 10 ? '0' + date.getHours() : date.getHours());
+  let m = String(date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+  let s = String(date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+
+  if (!format) return Y + M + D + ' ' + h + m + s;
+  let result = Y + '-' + M + '-' + D + ' ' + h + ':' + m + ':' + s;
+  return result;
+}
---
+++ web.js
@@ -0,0 +1,192 @@
+// 자바스크립트 엄격모드 ON
+'use strict';
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const db_config = require('./config/db.json');
+const db = mysql.createConnection(db_config);
+
+// 데이터베이스, 엔진 내보내기
+module.exports = { db, app }
+
+// 클라이언트 API 불러오기
+require('./clientAPI');
+
+// 엔진 셋팅
+app.set('port', 8001);
+app.use('/test', express.static(__dirname + '../client'));
+
+// 실행
+const { serverStart, dbConnect } = require('./main');
+db.connect(dbConnect);
+app.listen(app.get('port'), serverStart);
+
+// IP 필터 함수
+// const ipFilter = () => {
+//   const isTrue = db.query('SELECT (*) AS COUNT FROM hardware_list');
+//   return isTrue;
+// }
+
+// 현재 날씨 상태 정보 요청 (클라이언트 응답)
+// const _getNowWeather = (req, res) => {
+//   let x = req.params.x;
+//   let y = req.params.y;
+//   db.query(`
+//     SELECT * FROM now_weather WHERE NX=${x} AND NY=${y}
+//   `, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       log('클라이언트를 통해 초단기실황 데이터 요청에 실패하였습니다.');
+//       return;
+//     }
+//     log('클라이언트를 통해 초단기실황 데이터 요청에 성공하였습니다.');
+//     res.send(result);
+//   })
+// }
+
+// 장비 리스트 조회
+// const getHardwareList = (req, res) => {
+//   db.query(`
+//     SELECT * FROM hardware_list
+//   `, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       log('장비 리스트 조회 요청에 실패하였습니다.');
+//       return;
+//     }
+
+//     log('장비 리스트 조회에 성공하였습니다.');
+//     res.send(result);
+//   })
+// }
+
+// 장비 등록 (보류)
+// const _init = (req, res) => {
+
+//   return;
+//   let dong = req.params.dong;
+//   log(dong + ' 장비 등록을 요청합니다.');
+
+//   // 등록 여부 파악
+//   db.query(`
+//     SELECT * FROM hardware_list WHERE PATH3='${dong}';
+//   `, (err, result) => {
+//     if (err) {
+//       log(dong + ' 장비 등록 여부 조회에 실패하였습니다.');
+//       res.send({ result: false, info: err, msg: '장비 등록 여부 조회 오류' });
+//       return;
+//     }
+
+//     if (result.length > 0) {  // 이미 등록된 장비
+//       log(dong + ' 장비는 이미 등록된 장비입니다.');
+//       res.send({ result: false, info: result[0], msg: dong + ' 장비는 이미 등록된 장비입니다.' });
+//       return;
+
+//     } else {  // 신규 장비
+
+//       // DB의 동 정보 조회
+//       db.query(`
+//         SELECT * FROM location_list WHERE PATH3 LIKE '%${dong}%';
+//       `, (err, result) => {
+//         if (err) {
+//           log('동 위치 조회에 실패하였습니다.');
+//           res.send({ result: false, info: err, msg: '동 위치 조회 오류' });
+//           return;
+//         }
+
+//         if (result.length === 0) {
+//           res.send({ result: false, info: null, msg: '정확한 동 이름을 입력해주세요.' });
+//           return;
+//         }
+
+//         if (result.length > 1) return res.send({
+//           result: false, 
+//           info: '검색결과 => ' + result.map(item => item.PATH3), 
+//           msg: '동 이름을 정확하게 입력해주세요.'
+//         });
+
+//         let insertData = result[0];
+//         db.query(`
+//           INSERT INTO hardware_list (CODE,PATH1,PATH2,PATH3,NX,NY,CREATE_DATE) VALUES (
+//             '${insertData.CODE}',
+//             '${insertData.PATH1}',
+//             '${insertData.PATH2}',
+//             '${insertData.PATH3}',
+//             ${insertData.NX},
+//             ${insertData.NY},
+//             '${useNow()}'
+//           )
+//         `, (err, result) => {
+//           if (err) {
+//             log(dong + ' 장비 등록에 실패하였습니다.');
+//             res.send({ result: false, info: err, msg: '장비 등록 오류' });
+//             return;
+//           }
+          
+//           log(dong + ' 장비 등록에 성공하였습니다.');
+//           res.send({ 
+//             result: true, 
+//             info: {
+//               ID: insertData.ID,
+//               CODE: insertData.CODE,
+//               PATH1: insertData.PATH1,
+//               PATH2: insertData.PATH2,
+//               PATH3: insertData.PATH3,
+//               NX: insertData.NX,
+//               NY: insertData.NY,
+//             }, 
+//             msg: insertData.PATH3 + ' 장비가 등록되었습니다.' 
+//           });
+//         });
+        
+//       });
+//     }
+//   })
+// }
+
+// 장비 삭제 (보류)
+// const _remove = (req, res) => {
+//   return;
+  
+//   let dong = req.params.dong;
+//   log(dong + ' 장비 삭제을 요청합니다.');
+
+//   db.query(`
+//     SELECT * FROM hardware_list WHERE PATH3 LIKE '${dong}'
+//   `, (err, result) => {
+//     if (err) {
+//       log(dong + ' 장비 등록 여부 조회에 실패하였습니다.');
+//       res.send({ result: false, info: err, msg: '장비 등록 여부 조회 오류' });
+//       return;
+//     }
+
+//     if (result.length === 0) {
+//       res.send({ result: false, info: null, msg: '등록된 장비가 아닙니다.' });
+//       return;
+//     }
+
+//     if (result.length > 1) return res.send({
+//       result: false, 
+//       info: '검색결과 => ' + result.map(item => item.PATH3), 
+//       msg: '동 이름을 정확하게 입력해주세요.'
+//     });
+    
+//     let deleteData = result[0];
+//     db.query(`
+//       DELETE FROM hardware_list WHERE ID=${deleteData.ID}
+//     `, (err, result) => {
+//       if (err) {
+//         console.log(err);
+//         log(dong + ' 장비 삭제 요청에 실패하였습니다.');
+//         return;
+//       }
+      
+//       res.send({ 
+//         result: true, 
+//         info: deleteData, 
+//         msg: deleteData.PATH3 + ' 장비가 삭제되었습니다.' 
+//       });
+//     });
+//   });
+// }
