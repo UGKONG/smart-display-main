3978166ff408da5d67f4beeb06466e5ff4878c87
sanguk <jsw9330@icloud.com>
2022-04-03 16:34:37 +0900

    init

--- client.js
+++ client.js
@@ -4,21 +4,21 @@ const { db } = require('./web');
 
 module.exports = { dbIsConnect };
 
-const welcomeHTML = ({ ipAddress, result }) => {
+const indexHTML = ({ ipAddress, result }) => {
   const stateStyle = `background-color: ${result ? '#33db33' : '#ff4b4b'}`;
   const tag = `
     <style>
-      * { list-style: none; padding: 0; }
-      html, body { width: 100%; height: 100%; font-family: monospace; }
+      * { list-style: none; padding: 0; font-family: monospace; }
+      html, body { width: 100%; height: 100%; }
       body { color: #fff; background-color: #333; display: flex; align-items: center; justify-content: center; flex-flow: column; }
-      img { width: 300px; } h1 { font-size: 30px; } h2 {  text-align: center; }
-      li { margin-bottom: 4px; }
+      img { width: 300px; } h1 { font-size: 28px; }
+      li { margin-bottom: 6px; font-size: 24px; }
       span { display: inline-block; width: 60px; }
-      span.type { width: 80px; font-size: 14px; }
-      span.dot { width: 8px; height: 8px; border-radius: 100%; }
+      span.type { width: 120px; }
+      span.dot { width: 10px; height: 10px; border-radius: 100%; }
     </style>
     <body>
-      <img src='/logo.png' /><h1>Node Server</h1><h2>Access IP: ${ipAddress}</h2><h2 style='margin-bottom: 0;'>Connect State</h2>
+      <img src='/logo.png' /><h1>Access IP: ${ipAddress}</h1><h1 style='margin-bottom: 0;'>Connect State</h1>
       <ul>
         <li><span class='type'>Server</span><span class='dot' style='${stateStyle}'></span></li>
         <li><span class='type'>MySQL</span><span class='dot' style='${stateStyle}'></span></li>
@@ -32,6 +32,47 @@ function dbIsConnect (req, res) {
   db.query('SELECT * FROM test', (err) => {
     const ipAddress = requestIp.getClientIp(req);
     let result = err ? false : true;
-    res.send(welcomeHTML({ ipAddress, result }));
+    res.send(indexHTML({ ipAddress, result }));
   })
 }
+
+
+
+// IP 필터 함수
+// const ipFilter = () => {
+//   const isTrue = db.query('SELECT (*) AS COUNT FROM hardware_list');
+//   return isTrue;
+// }
+
+// 현재 날씨 상태 정보 요청 (클라이언트 응답)
+// const _getNowWeather = (req, res) => {
+//   let x = req.params.x;
+//   let y = req.params.y;
+//   db.query(`
+//     SELECT * FROM now_weather WHERE NX=${x} AND NY=${y}
+//   `, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       log('클라이언트를 통해 초단기실황 데이터 요청에 실패하였습니다.');
+//       return;
+//     }
+//     log('클라이언트를 통해 초단기실황 데이터 요청에 성공하였습니다.');
+//     res.send(result);
+//   })
+// }
+
+// 장비 리스트 조회
+// const getHardwareList = (req, res) => {
+//   db.query(`
+//     SELECT * FROM hardware_list
+//   `, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       log('장비 리스트 조회 요청에 실패하였습니다.');
+//       return;
+//     }
+
+//     log('장비 리스트 조회에 성공하였습니다.');
+//     res.send(result);
+//   })
+// }
