51164d60a875a38c8c3824852dc95498985f8f40
sanguk <jsw9330@icloud.com>
2022-04-07 18:11:44 +0900

    1.0.2

--- web.js
+++ web.js
@@ -7,6 +7,7 @@ const app = express();
 const mysql = require('mysql');
 const db_config = require('./server/json/db.json');
 const db = mysql.createConnection(db_config);
+const port = 8001;
 
 // 서버엔진, 데이터베이스 내보내기
 module.exports = { db, app };
@@ -15,12 +16,12 @@ global.db = db;
 
 // 엔진 셋팅
 app.use(cors());
-app.use(express.static(__dirname + '/../client/build'));
+app.use('/', express.static(__dirname + '/client/build'));
 
 // 실행
 const { serverStart, dbConnect } = require('./server/main');
 db.connect(dbConnect);
-app.listen(8001, '0.0.0.0', serverStart);
+app.listen(port, '0.0.0.0', serverStart);
 
 const {
   isConnect, 
@@ -29,6 +30,7 @@ const {
 } = require('./server/api');
 
 // API
+// app.get('/', (req, res) => res.sendFile('/'));
 app.get('/api/isConnect', isConnect);
 app.get('/api/log', log);
 app.get('*', pageNotFound);
